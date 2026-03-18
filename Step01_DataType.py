4121ad6dd089d1c16684da114a207679ff8b4c75
youngsik <dudtlr9308@gmail.com>
2026-03-18 10:41:24 +0900

    DatatType.py도 추가함

---
+++ Step01_DataType.py
@@ -0,0 +1,65 @@
+# 한줄 주석입니다.
+
+"""
+
+여러 줄 주석 테스트 ㅋㅋ
+
+"""
+
+print('Step01 시작')
+
+# python 의 여러가지 데이터 type에 대해서 알아보자
+
+# int type 
+num1 = 10
+age = 32
+
+# float type
+num2 = 10.1
+
+# str type
+myName = '영식'
+
+ourName = """
+이거는 어떻게 
+출력이 될까 ?
+"""
+
+
+print(" 제 이름은 " + myName + " 입니다." + " 그리고 나이는 " , age , " 입니다")
+
+
+print(ourName)
+
+
+foods = ['삼겹살', '김밥', '닭발']
+
+print(' 몇번 째 음식 인가 ',foods[2])
+
+for food in foods:
+    print (food)
+
+
+memInfo = {'num':1  , 'name' : 'youngsik', 'addr':  '용인' }
+
+print('뻘 건 점 찍기 전 ')
+
+
+print(memInfo)
+
+print (memInfo['num'])
+
+print (' -function made-- ')
+
+def store() :
+    print('냉장고 문을 연다')
+    print('물건을 저장한다.')
+    print('냉장고 문을 닫는다')
+    return True
+    
+print(store())
+
+chatgpt = None
+
+
+print('현재는 빈 값인가요 ? ',chatgpt)
